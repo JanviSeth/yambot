a4bc353bd8fe6c4ecbf349b25872b2fe7c8d0f17
Janvi <janviseth@hotmail.com>
2024-09-26 16:16:39 +0200

    launch edit

--- src/yambot/launch/spawn_robot.launch.py
+++ src/yambot/launch/spawn_robot.launch.py
@@ -7,11 +7,14 @@ def generate_launch_description():
             package='gazebo_ros',
             executable='spawn_entity.py',
             arguments=[
-                '--entity', 'my_robot',
-                '--robot_namespace', 'my_robot',
-                '--file', '<path_to_your_urdf>',
-                '--ros-args', '--param', 'use_sim_time:=true'
+                '--entity', 'my_robot',  # Entity name for your robot
+                '--robot_namespace', 'my_robot',  # Namespace for the robot
+                '--file', '/home/janvi/yambot_ws/src/yambot/urdf/my_robot.urdf',  # Path to your URDF
+                '--unpause',  # Optionally unpause the simulation
+                # Moved '--ros-args' here
+                '--ros-args', '--param', 'use_sim_time:=true'  # Set simulation time parameter
             ],
             output='screen',
         ),
     ])
+
